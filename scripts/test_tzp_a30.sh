67898656620df9d83954afe57957fdd01ee10f52
PlagaMedicum <katoshrodingera@protonmail.ch>
2026-03-07 18:16:50 +0100

    build: isolate tzp packet lane selectors per pytest call

--- scripts/test_tzp_a30.sh
+++ scripts/test_tzp_a30.sh
@@ -6,7 +6,20 @@ ensure_venv
 
 pytest_run -q -o addopts='' \
   tests/test_tzp_comment_policy.py \
+  "$@"
+
+pytest_run -q -o addopts='' \
   tests/test_saver.py -k "tzp_comment_writeback" \
+  "$@"
+
+pytest_run -q -o addopts='' \
   tests/test_file_workflow.py -k "persist_current_save or write_from_cache" \
+  "$@"
+
+pytest_run -q -o addopts='' \
   tests/test_parser_features.py -k "status_comment" \
+  "$@"
+
+pytest_run -q -o addopts='' \
+  tests/test_gui_tm_preferences.py -k "tzp_writeback" \
   "$@"
